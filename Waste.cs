b597ed04b50214fc5ffab644d5e84ecc0afea7dd
long21wt <46249208+long21wt@users.noreply.github.com>
2022-12-01 09:28:43 -0800

    Initial commit

---
+++ Waste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demolition_Planing_Tool
+{
+    internal class Waste
+    {
+    }
+}
