ec145067f3d92b987a9306e4754cf5bbfb1798f3
long21wt <46249208+long21wt@users.noreply.github.com>
2023-01-28 19:57:45 +0100

    Update to connect to Revit

---
+++ App.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+
+namespace Demolition_Planing_Tool
+{
+    public class App : IExternalApplication
+    {
+        static string thisAssemblyPath = typeof(App).Assembly.Location;//path: the loaction is ../bin/Debug folder
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            String panelName = "Open Demolition Planning Tool"; // the name of panel
+
+            RibbonPanel ribbonDemoPanel = application.CreateRibbonPanel(panelName); // create a new RibbonPanel on the Addin tab
+
+            PushButton myButton = (PushButton)ribbonDemoPanel.AddItem(new PushButtonData(
+                "Demolition Planning Tool",
+                "Demolition Planning Tool",
+                thisAssemblyPath,
+                "Demolition_Planing_Tool.CustomControl1")
+            );
+
+            //set the icon of button
+            string ButtonIconsFolder = Path.GetDirectoryName(thisAssemblyPath); //path: the loaction is ../bin/Debug folder
+
+            myButton.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder, "tiger_35x35.png"), UriKind.Absolute)); //choose the .png with pixel 35x35. The size of the pixel is more appropriate
+
+            myButton.ToolTip = "Click to get the tiger image"; //tip of button
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
+
+
---
+++ Command.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using iText.Kernel.XMP.Impl;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demolition_Planing_Tool
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class CustomControl1 : IExternalCommand
+    {
+        private static UIDocument uidoc;
+        private static Document doc;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            uidoc = uiApp.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ElementCategoryFilter filterFloor = new ElementCategoryFilter(BuiltInCategory.OST_Floors);
+            IList<Element> floorList = collector.WherePasses(filterFloor).WhereElementIsNotElementType().ToElements();
+            Debug.WriteLine(floorList.Count);
+
+            foreach (Element floor in floorList)
+            {
+                Debug.WriteLine(floor.ToString());
+            }
+            Building b = new Building("", "", "", "", floorList.Count, 0);
+            ComputeForm dialog = new ComputeForm(b, floorList.Count);
+            dialog.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
